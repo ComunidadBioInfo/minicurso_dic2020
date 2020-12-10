d49f0760630cc8d61c50c592ea1eefed94172b72
Erick Cuevas <erickcufe94@gmail.com>
2020-12-09 19:19:23 -0600

    Script con ejemplos para la clase

---
+++ Intro_Rstudio.R
@@ -0,0 +1,56 @@
+# Esto es un comentario
+
+# Funciones basicas
+
+# Suma
+1000 + 123
+
+# Resta
+233 - 30
+
+# Exponente
+2**3
+2^3
+
+# Division
+125 / 25
+
+# Multiplicacion
+5 * 9
+
+# Notaci??n cientifica
+1E-8
+1e-8
+
+# Raiz cuadrada
+sqrt(8)
+
+# Sumatoria
+sum( c(1,2,3,4,5,6,7,8,9) )
+
+
+# Tipos de variables
+
+# Numericas
+x <- 1
+y <- 1.2
+z <- 3.1416
+class(x)
+
+# Logicas
+a <- TRUE
+b <- FALSE
+a == b
+a != b
+x == y
+x < y
+class(a)
+
+# Caracter
+ejemplo <- "abcde"
+class(ejemplo)
+
+
+
+
+
